131c1bae37b9d62a2dc00cb9dd856964ea7280d7
hyunseo99 <71087310+hyunseo99@users.noreply.github.com>
2020-12-15 00:03:20 +0900

    Update relay.py

--- relay.py
+++ relay.py
@@ -1,22 +1,22 @@
 import RPi.GPIO as GPIO
+import time
 
-#GPIO.HIGH / LOW => OFF = 1 ON = 0
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-air_GPIO = 23 #공기 청정기
-dehumid_GPIO = 24 #제습기
-window_GPIO = 25 #창문 모터
-
-GPIO.setup(18,GPIO.OUT) #GROUND
-
-GPIO.setup(air_GPIO, GPIO.OUT)
-GPIO.setup(dehumid_GPIO, GPIO.OUT)
-GPIO.setup(window_GPIO, GPIO.OUT)
-
-#공기청정기 1, 제습기 2, 창문모터 3
+#공기청정기 1, 제습기 2
 #on/off 각각 1, 0
-def relay(self, ctrl, onoff):
+def relay(ctrl, onoff):
+    #GPIO.HIGH / LOW => OFF = 1 ON = 0
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    air_GPIO = 23 #공기 청정기
+    dehumid_GPIO = 24 #제습기
+
+    GPIO.setup(18,GPIO.OUT) #GROUND
+
+    GPIO.setup(air_GPIO, GPIO.OUT)
+    GPIO.setup(dehumid_GPIO, GPIO.OUT)
+    GPIO.setup(window_GPIO, GPIO.OUT)
+    
+    #GPIO.HIGH / LOW => OFF = 1 ON = 0
     if (ctrl == 1 and onoff == 0):
         GPIO.output(air_GPIO, GPIO.LOW)
         print("Air Purifier  OFF")
@@ -29,11 +29,16 @@ def relay(self, ctrl, onoff):
     elif (ctrl == 2 and onoff == 1):
         GPIO.output(dehumid_GPIO, GPIO.HIGH)
         print("Dehumidifier  ON")
-    elif (ctrl == 3 and onoff == 0):
-        GPIO.output(window_GPIO, GPIO.LOW)
-        print("Window OFF")
-    elif (ctrl == 3 and onoff == 1):
-        GPIO.output(window_GPIO, GPIO.HIGH)
-        print("Window ON")
-        
-GPTO.cleanup()
+
+    GPTO.cleanup()
+
+if __name__ == '__main__':
+    relay(1, 0) # 공기청정기 켜짐
+    time.sleep(5)
+    relay(1, 0) # 공기청정기 꺼짐
+
+    time.sleep(2)
+
+    relay(2, 0) # 제습기 켜짐
+    time.sleep(5)
+    relay(2, 0) # 제습기 꺼짐
